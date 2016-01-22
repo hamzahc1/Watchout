dbd11c6f40d6e0d0c660c6faf46837759700a1ab
hamzahc1 <hamzahchaudhary@gmail.com>
2016-01-22 15:50:23 -0800

    Implement create enemies and random movement

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,36 @@
 // start slingin' some d3 here.
+var dataSet = [1, 2, 3, 4];
+
+var enemies = d3.select(".board").selectAll("svg")
+	.data(dataSet);
+
+var createEnemies = function(){
+
+	enemies.enter()
+		.append("svg")
+		.attr('width', '25px')
+		.attr('height', '25px')
+		.style("top", function(){
+			return Math.random()*475 + 'px';
+		})
+		.style("left", function(){
+			return Math.random()*475 + 'px';
+		})
+		.append("image")
+		.attr("xlink:href", "asteroid.png")
+		.attr('width', '25px')
+		.attr('height', '25px');
+};
+
+var updateLocation = function() {
+	enemies
+		.style('top', function() {
+			return Math.random()*475 + 'px';
+		})
+		.style('left', function() {
+			return Math.random()*475 + 'px';
+		});
+};
+
+createEnemies();
+setInterval(updateLocation, 1000);
