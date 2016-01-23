e80c5b8bb8aea8522283fcc7e2609eb4d062fdf2
hamzahc1 <hamzahchaudhary@gmail.com>
2016-01-22 21:11:12 -0800

    Implement collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -1,7 +1,10 @@
 // start slingin' some d3 here.
+var RADIUS = 12.5;
+var BOARD_SIZE = 500;
+
 var enemyList = [1, 2, 3, 4];
 
-var playerList = [{'top': '50px', 'left': '50px'}];
+var playerList = [{'width': 500, 'height': 500, 'r': 12.5}];
 
 var enemies = d3.select(".board").selectAll("svg.enemies")
 	.data(enemyList);
@@ -9,33 +12,47 @@ var enemies = d3.select(".board").selectAll("svg.enemies")
 var player =d3.select(".mouse").selectAll("svg.player")
 		.data(playerList);
 
+// var scoreBoard = d3.select("scoreBoard").selectAll("div")
+
 var drag = d3.behavior.drag()
+	// .on("dragstart", function(){
+	// 	player.style("fill", "red");
+	// })
 	.on("drag", function(){
-		var dx = d3.event.x - 250;
-		var dy = d3.event.y - 250;
-		d3.select(this).attr("transform", "translate(" + dx + ", " + dy + ")");
-		// player.style("top", d3.event.y - 12.5).style("left", d3.event.x - 12.5);
+		var dx = d3.event.x;
+		var dy = d3.event.y;
+
+		dx = dx < 487.5 ? dx : 487.5;
+		dx = dx > 12.5 ? dx : 12.5;
+
+		dy = dy < 487.5 ? dy : 487.5;
+		dy = dy > 12.5 ? dy : 12.5;
+
+		//d3.select(this).attr("transform", "translate(" + dx + ", " + dy + ")");
+		player.select('circle').attr("cy", dy).attr("cx", dx);
 	});
-	// .on("dragend", function(){
-	// 	player.style("fill", "black");
-	// });
+
 
 var createPlayer = function(){
 	player.enter()
 		.append("svg")
-		.attr("width", "500px")
-		.attr("height", "500px")
-		// .style("top", function(d){
-		// 	return d.top;
-		// })
-		// .style("left", function(d){
-		// 	return d.left;
-		// })
+		.attr("width", function(d) {
+			return d.width;
+		})
+		.attr("height", function(d) {
+			return d.height;
+		})
 		.classed("player", true)
 		.append("circle")
-		.attr("cx", "250px")
-		.attr("cy", "250px")
-		.attr("r", "12.5px")
+		.attr("cx", function(d) {
+			return d.width/2;
+		})
+		.attr("cy", function(d) {
+			return d.height/2;
+		})
+		.attr("r", function(d) {
+			return d.r;
+		})
 		.attr("cursor", "pointer")
 		.call(drag);
 };
@@ -44,35 +61,74 @@ var createEnemies = function(){
 
 	enemies.enter()
 		.append("svg")
-		.attr('width', '25px')
-		.attr('height', '25px')
+		.attr('width', 25)
+		.attr('height', 25)
 		.style("top", function(){
-			return Math.random()*475 + 'px';
+			return Math.floor(Math.random()*475);
 		})
 		.style("left", function(){
-			return Math.random()*475 + 'px';
+			return Math.floor(Math.random()*475);
 		})
 		.classed('enemies', true)
 		.append("image")
 		.attr("xlink:href", "asteroid.png")
 		.attr('width', '25px')
-		.attr('height', '25px');
+		.attr('height', '25px'); 
+};
+
+var checkCollision = function(enemyX, enemyY, playerX, playerY) {
+	var xDiff = Math.abs(enemyX - playerX);
+	var yDiff = Math.abs(enemyY - playerY);
+
+	return xDiff < RADIUS*2 && yDiff < RADIUS*2;
 };
 
 var updateLocation = function() {
+	var generate = function(n){
+		var result = [];
+		for(var i = 0; i<n; i++){
+			result.push(Math.floor(Math.random()*475));
+		}
+		return result;
+	};
+	arrX = generate(enemyList.length);
+	arrY = generate(enemyList.length);
+
 	enemies
-		.style('top', function() {
-			return Math.random()*475 + 'px';
+		.transition()
+		.tween("custom", function(d,i){
+			var oldX = Number(this.style.left.slice(0,-2));
+			var oldY = Number(this.style.top.slice(0,-2));
+
+			var xInterpolater = d3.interpolate(oldX, arrX[i]);
+			var yInterpolater = d3.interpolate(oldY, arrY[i]);
+			return function(t){
+				var currentX = xInterpolater(t);
+				var currentY = yInterpolater(t);
+
+				var playerX = player.select('circle').attr('cx');
+				var playerY = player.select('circle').attr('cy');
+
+				if (checkCollision(currentX+RADIUS, currentY+RADIUS, playerX, playerY)) {
+					player.style("fill","red");
+				}
+			};
+
 		})
-		.style('left', function() {
-			return Math.random()*475 + 'px';
-		});
+		.style('top', function(d,i) {
+			return arrY[i];
+		})
+		.style('left', function(d,i) {
+			return arrX[i];
+		})
+		.duration(750)
+		.delay(1000);
 };
 
 
 createPlayer();
 createEnemies();
-setInterval(updateLocation, 1000);
+setInterval(updateLocation, 1200);
 
 
 // player.call(drag);
