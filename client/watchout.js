aaa2ef45089ec51d40b1bb9a642c1fa6a04400e4
hamzahc1 <hamzahchaudhary@gmail.com>
2016-01-22 16:18:07 -0800

    Add player dot

--- client/watchout.js
+++ client/watchout.js
@@ -1,8 +1,30 @@
 // start slingin' some d3 here.
-var dataSet = [1, 2, 3, 4];
+var enemyList = [1, 2, 3, 4];
 
-var enemies = d3.select(".board").selectAll("svg")
-	.data(dataSet);
+var playerList = [{'top': '50px', 'left': '50px'}];
+
+var enemies = d3.select(".board").selectAll("svg.enemies")
+	.data(enemyList);
+
+var createPlayer = function(){
+	d3.select(".mouse").selectAll("svg.player")
+		.data(playerList)
+		.enter()
+		.append("svg")
+		.attr("width", "25px")
+		.attr("height", "25px")
+		.style("top", function(d){
+			return d.top;
+		})
+		.style("left", function(d){
+			return d.left;
+		})
+		.classed("player", true)
+		.append("circle")
+		.attr("cx", "12.5px")
+		.attr("cy", "12.5px")
+		.attr("r", "12.5px");
+};
 
 var createEnemies = function(){
 
@@ -16,6 +38,7 @@ var createEnemies = function(){
 		.style("left", function(){
 			return Math.random()*475 + 'px';
 		})
+		.classed('enemies', true)
 		.append("image")
 		.attr("xlink:href", "asteroid.png")
 		.attr('width', '25px')
@@ -32,5 +55,6 @@ var updateLocation = function() {
 		});
 };
 
+createPlayer();
 createEnemies();
 setInterval(updateLocation, 1000);
